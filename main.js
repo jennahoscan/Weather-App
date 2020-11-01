3baed2226f98806f58c5d0c0cee2cd1926eb7306
jennarayman <jrayman@mica.edu>
2020-11-01 13:42:18 -0500

    Added new functionality

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Get Current Time and Update Every 2 Seconds
+// Get Current Time and Update Every Second
 setInterval(function () {
   let date = new Date();
 
@@ -11,61 +11,174 @@ setInterval(function () {
 
   let finalTime = `${hours}:${minutes} ${amOrPm}`;
 
-  document.querySelector("span").innerHTML = finalTime;
-}, 2000);
+  document.querySelector("#time").innerHTML = finalTime;
+}, 1000);
 
 //Get Current Day of the Week, Month, and Date
-let date = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-const daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let dayNumber = date.getDay();
-let dayName = daysOfWeek[dayNumber];
+  let dayNumber = date.getDay();
+  let dayName = daysOfWeek[dayNumber];
 
-const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-let monthNumber = date.getMonth();
-let monthName = monthNames[monthNumber];
+  let monthNumber = date.getMonth();
+  let monthName = monthNames[monthNumber];
 
-let day = date.getDate();
+  let day = date.getDate();
 
-let currentDate = `${dayName}, ${monthName} ${day}`;
+  let currentDate = `${dayName}, ${monthName} ${day}`;
 
-document.querySelector("h2").innerHTML = currentDate;
+  return currentDate;
+}
+
+let now = new Date();
+document.querySelector("#date").innerHTML = formatDate(now);
 
 // Update City Name on Search
 function search(event) {
   event.preventDefault();
   let userInput = document.querySelector("#search-input");
-  document.querySelector("h1").innerHTML = userInput.value;
+  document.querySelector("#city").innerHTML = userInput.value;
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
 
-//Convert Between Celsius and Farenheit
+//Get Forecast
+function getTodaysForecast(response) {
+  let temperature = document.querySelector("#degree");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
 
+  let hours = new Date().getHours();
+  let icon = document.querySelector("#icon");
+  let value = response.data.weather[0].id;
+  let code = response.data.weather[0].icon;
+  if (value > 199 && value < 233) {
+    icon.setAttribute("src", "Images/Thunderstorm.png");
+  } else if (value > 299 && value < 322) {
+    icon.setAttribute("src", "Images/Rain.png");
+  } else if (value > 499 && value < 505 && code === "10d") {
+    icon.setAttribute("src", "Images/DayRain.png");
+  } else if (value > 499 && value < 505 && hours > 18 && hours < 5) {
+    icon.setAttribute("src", "Images/NightRain.png");
+  } else if (value === 511) {
+    icon.setAttribute("src", "Images/Hail.png");
+  } else if (value > 519 && value < 532 && code === "09d") {
+    icon.setAttribute("src", "Images/DayRain.png");
+  } else if (value > 519 && value < 532 && hours > 18 && hours < 5) {
+    icon.setAttribute("src", "Images/NightRain.png");
+  } else if (value > 599 && value < 623) {
+    icon.setAttribute("src", "Images/Snow.png");
+  } else if (value > 710 && value < 742) {
+    icon.setAttribute("src", "Images/Wind.png");
+  } else if (value === 701) {
+    icon.setAttribute("src", "Images/Mist.png");
+  } else if (value === 771 || value === 781) {
+    icon.setAttribute("src", "Images/Wind.png");
+  } else if (value > 710 && value < 742) {
+    icon.setAttribute("src", "Images/Wind.png");
+  } else if (value === 800 && code === "01d") {
+    icon.setAttribute("src", "Images/Sun.png");
+  } else if (value === 800 && code === "01n") {
+    icon.setAttribute("src", "Images/Moon.png");
+  } else if (value === 801 && code === "02d") {
+    icon.setAttribute("src", "Images/DayCloud.png");
+  } else if (value === 801 && code === "02n") {
+    icon.setAttribute("src", "Images/NightCloud.png");
+  } else if (value === 802) {
+    icon.setAttribute("src", "Images/OneCloud.png");
+  } else if (value === 803 || value === 804) {
+    icon.setAttribute("src", "Images/Clouds.png");
+  }
+
+  fiveDayForecast(response);
+}
+
+function searchCity(city) {
+  let units = "metric";
+  let apiKey = "5ed4231e4848c446bb7ab760ec22172f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(getTodaysForecast);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+searchCity("Baltimore");
+
+//Get User's Current Location
+function locateUser() {
+  function getPosition(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let units = "metric";
+    let apiKey = "5ed4231e4848c446bb7ab760ec22172f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showCurrentLocationTemp);
+  }
+
+  navigator.geolocation.getCurrentPosition(getPosition);
+
+  function showCurrentLocationTemp(response) {
+    let city = document.querySelector("#city");
+    city.innerHTML = response.data.name;
+    let temperature = Math.round(response.data.main.temp);
+    let degree = document.querySelector("#degree");
+    degree.innerHTML = temperature;
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.weather[0].description;
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = response.data.main.humidity;
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = Math.round(response.data.wind.speed);
+
+    fiveDayForecast(response);
+  }
+}
+
+let button = document.querySelector("#text");
+button.addEventListener("click", locateUser);
+
+//Convert Between Celsius and Farenheit
 function convertToFarenheit(event) {
   event.preventDefault();
   let tempElement = document.querySelector("#degree");
@@ -86,10 +199,208 @@ function convertToCelsius(event) {
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", convertToCelsius);
 
-//Update Background Gradient Based on Time
-/*function colorChange (hour, amOrPm){
-  let body = document.querySelector("body");
-  if (hour >= 6 && amOrPm === "AM"){
-    body.style.background =
+//Change Background Gradient by Time
+var today = new Date().getHours();
+let searchBar = document.querySelector("#magnify");
+if (today >= 0 && today <= 2) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to top, #1b2c47 0%, #000000 100%)";
+  searchBar.setAttribute("src", "Images/magnify000.png");
+} else if (today === 3 || today === 4) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to top, #225a85 0%, #1b2c47 100%)";
+  searchBar.setAttribute("src", "Images/magnify1b2c47.png");
+} else if (today === 5 || today === 6) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to top, #9598f0 0%, #225a85 100%)";
+  searchBar.setAttribute("src", "Images/magnify225a85.png");
+} else if (today === 7) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to bottom, #9598f0 0%, #a1c4fd 100%)";
+  searchBar.setAttribute("src", "Images/magnify9598f0.png");
+} else if (today >= 8 && today <= 11) {
+  document.body.style.backgroundImage =
+    "linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)";
+  searchBar.setAttribute("src", "Images/magnifyc2e9fb.png");
+} else if (today >= 12 && today <= 14) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to top, #7aadff 0%, #c2e9fb 100%)";
+  searchBar.setAttribute("src", "Images/magnifyc2e9fb.png");
+} else if (today === 15 || today === 16) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to top, #a1c4fd 0%, #7aadff 100%)";
+  searchBar.setAttribute("src", "Images/magnify7aadff.png");
+} else if (today === 17 || today === 18) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to bottom, #a1c4fd 0%, #5d4ba1 100%)";
+  searchBar.setAttribute("src", "Images/magnifya1c4fd.png");
+} else if (today === 19) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to bottom, #5d4ba1 0%, #032845 100%)";
+  searchBar.setAttribute("src", "Images/magnify5d4ba1.png");
+} else if (today === 20 || today === 21) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to bottom, #394475 0%, #000000 100%)";
+  searchBar.setAttribute("src", "Images/magnify394475.png");
+} else if (today === 22 || today === 23) {
+  document.body.style.backgroundImage =
+    "linear-gradient(to bottom, #000000 0%, #1C1C1C 150%), linear-gradient(to top, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.25) 200%)";
+  searchBar.setAttribute("src", "Images/magnify000.png");
+}
+
+//Use Arrows to Click Through Forecast
+function fiveDayForecast(response) {
+  let lat = response.data.coord.lat;
+  let lon = response.data.coord.lon;
+  let apiKey = "5ed4231e4848c446bb7ab760ec22172f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${apiKey}`;
+  axios.get(apiUrl).then(getWeeklyForecast);
+}
+
+function getWeeklyForecast(response) {
+  let pageNumber = 6;
+
+  const nextTag = document.querySelector("#right-arrow");
+  const prevTag = document.querySelector("#left-arrow");
+  const degreeTag = document.querySelector("#degree");
+  const descriptionTag = document.querySelector("#description");
+  const humidityTag = document.querySelector("#humidity");
+  const windTag = document.querySelector("#wind");
+  const dateTag = document.querySelector("#date");
+
+  const pages = [
+    {
+      date: formatDate(response.data.daily[1].dt * 1000),
+      degree: Math.round(response.data.daily[1].temp.day - 273.15),
+      description: response.data.daily[1].weather[0].description,
+      humidity: response.data.daily[1].humidity,
+      wind: Math.round(response.data.daily[1].wind_speed),
+    },
+    {
+      date: formatDate(response.data.daily[2].dt * 1000),
+      degree: Math.round(response.data.daily[2].temp.day - 273.15),
+      description: response.data.daily[2].weather[0].description,
+      humidity: response.data.daily[2].humidity,
+      wind: Math.round(response.data.daily[2].wind_speed),
+    },
+    {
+      date: formatDate(response.data.daily[3].dt * 1000),
+      degree: Math.round(response.data.daily[3].temp.day - 273.15),
+      description: response.data.daily[3].weather[0].description,
+      humidity: response.data.daily[3].humidity,
+      wind: Math.round(response.data.daily[3].wind_speed),
+    },
+    {
+      date: formatDate(response.data.daily[4].dt * 1000),
+      degree: Math.round(response.data.daily[4].temp.day - 273.15),
+      description: response.data.daily[4].weather[0].description,
+      humidity: response.data.daily[4].humidity,
+      wind: Math.round(response.data.daily[4].wind_speed),
+    },
+    {
+      date: formatDate(response.data.daily[5].dt * 1000),
+      degree: Math.round(response.data.daily[5].temp.day - 273.15),
+      description: response.data.daily[5].weather[0].description,
+      humidity: response.data.daily[5].humidity,
+      wind: Math.round(response.data.daily[5].wind_speed),
+    },
+    {
+      date: formatDate(response.data.daily[6].dt * 1000),
+      degree: Math.round(response.data.daily[6].temp.day - 273.15),
+      description: response.data.daily[6].weather[0].description,
+      humidity: response.data.daily[6].humidity,
+      wind: Math.round(response.data.daily[6].wind_speed),
+    },
+    {
+      date: formatDate(response.data.current.dt * 1000),
+      degree: Math.round(response.data.current.temp - 273.15),
+      description: response.data.current.weather[0].description,
+      humidity: response.data.current.humidity,
+      wind: Math.round(response.data.current.wind_speed),
+    },
+  ];
+
+  function updateSection() {
+    console.log(response.data);
+    dateTag.innerHTML = pages[pageNumber].date;
+    degreeTag.innerHTML = pages[pageNumber].degree;
+    descriptionTag.innerHTML = pages[pageNumber].description;
+    humidityTag.innerHTML = pages[pageNumber].humidity;
+    windTag.innerHTML = pages[pageNumber].wind;
+    let hours = new Date().getHours();
+    let icon = document.querySelector("#icon");
+    let value = response.data.daily[pageNumber].weather[0].id;
+    let code = response.data.daily[pageNumber].weather[0].icon;
+    if (value > 199 && value < 233) {
+      icon.setAttribute("src", "Images/Thunderstorm.png");
+    } else if (value > 299 && value < 322) {
+      icon.setAttribute("src", "Images/Rain.png");
+    } else if (value > 499 && value < 505 && code === "10d") {
+      icon.setAttribute("src", "Images/DayRain.png");
+    } else if (value > 499 && value < 505 && hours > 18 && hours < 5) {
+      icon.setAttribute("src", "Images/NightRain.png");
+    } else if (value === 511) {
+      icon.setAttribute("src", "Images/Hail.png");
+    } else if (value > 519 && value < 532 && code === "09d") {
+      icon.setAttribute("src", "Images/DayRain.png");
+    } else if (value > 519 && value < 532 && hours > 18 && hours < 5) {
+      icon.setAttribute("src", "Images/NightRain.png");
+    } else if (value > 599 && value < 623) {
+      icon.setAttribute("src", "Images/Snow.png");
+    } else if (value > 710 && value < 742) {
+      icon.setAttribute("src", "Images/Wind.png");
+    } else if (value === 701) {
+      icon.setAttribute("src", "Images/Mist.png");
+    } else if (value === 771 || value === 781) {
+      icon.setAttribute("src", "Images/Wind.png");
+    } else if (value > 710 && value < 742) {
+      icon.setAttribute("src", "Images/Wind.png");
+    } else if (value === 800 && code === "01d") {
+      icon.setAttribute("src", "Images/Sun.png");
+    } else if (value === 800 && code === "01n") {
+      icon.setAttribute("src", "Images/Moon.png");
+    } else if (value === 801 && code === "02d") {
+      icon.setAttribute("src", "Images/DayCloud.png");
+    } else if (value === 801 && code === "02n") {
+      icon.setAttribute("src", "Images/NightCloud.png");
+    } else if (value === 802) {
+      icon.setAttribute("src", "Images/OneCloud.png");
+    } else if (value === 803 || value === 804) {
+      icon.setAttribute("src", "Images/Clouds.png");
+    }
   }
-}*/
+
+  const next = function () {
+    pageNumber = pageNumber + 1;
+    if (pageNumber > pages.length - 1) {
+      pageNumber = 0;
+    }
+    updateSection();
+  };
+
+  const prev = function () {
+    pageNumber = pageNumber - 1;
+    if (pageNumber < 0) {
+      pageNumber = pages.length - 1;
+    }
+    updateSection();
+  };
+
+  nextTag.addEventListener("click", function () {
+    next();
+  });
+
+  prevTag.addEventListener("click", function () {
+    prev();
+  });
+
+  document.addEventListener("keyup", function (event) {
+    console.log(event);
+    if (event.key === "ArrowRight") {
+      next();
+    }
+    if (event.key === "ArrowLeft") {
+      prev();
+    }
+  });
+}
